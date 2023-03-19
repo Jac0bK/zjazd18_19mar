e2a931ea0bd63cdb6c20496737844ddbb8e0df57
student-wroclaw <j.kostansky@gmail.com>
2023-03-19 11:34:24 +0100

    test Ok zmieniony local host i dodany nowy test

--- android phone apidemo.py
+++ android phone apidemo.py
@@ -18,7 +18,7 @@ class TestApiDemos(unittest.TestCase):
         caps = {}
         caps["platformName"] = "Android"
         caps["appium:platformVersion"] = "9"
-        caps["appium:deviceName"] = "localhost:50640"
+        caps["appium:deviceName"] = "localhost:52893"
         caps["appium:app"] = "C:\\Users\\vdi-student\\Downloads\\ApiDemos-debug.apk"
         caps["appium:ensureWebviewsHavePages"] = True
         caps["appium:nativeWebScreenshot"] = True
@@ -30,7 +30,7 @@ class TestApiDemos(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_api_demos(self):
+    def test_api_demos_1(self):
         el1 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="App")
         el1.click()
         el2 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Menu")
@@ -40,6 +40,19 @@ class TestApiDemos(unittest.TestCase):
         # if el3.text == "Inflate from XML":
         #     print("SUCCESS")
         self.assertEqual(el3.text, "Inflate from XML")
+
+    def test_api_demos_2(self):
+        el1 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="App")
+        el1.click()
+        el2 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Menu")
+        el2.click()
+        el3 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Inflate from XML")
+        el3.click()
+        title_inflate_from_xml = self.driver.find_element(
+            by=AppiumBy.XPATH,
+            value="/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TextView"
+        )
+        self.assertEqual(title_inflate_from_xml.text, "App/Menu/Inflate from XML")
 
 
 if __name__ == "__main__":
