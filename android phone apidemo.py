017aefdef89394c849be75a47df140732336f4a1
student-wroclaw <j.kostansky@gmail.com>
2023-03-19 12:46:05 +0100

    drag and drop

--- android phone apidemo.py
+++ android phone apidemo.py
@@ -1,7 +1,7 @@
 # This sample code uses the Appium python client v2
 # pip install Appium-Python-Client
 # Then you can paste this into a file and simply run with Python
-
+import time
 import unittest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
@@ -18,7 +18,7 @@ class TestApiDemos(unittest.TestCase):
         caps = {}
         caps["platformName"] = "Android"
         caps["appium:platformVersion"] = "9"
-        caps["appium:deviceName"] = "localhost:52893"
+        caps["appium:deviceName"] = "localhost:54100"
         caps["appium:app"] = "C:\\Users\\vdi-student\\Downloads\\ApiDemos-debug.apk"
         caps["appium:ensureWebviewsHavePages"] = True
         caps["appium:nativeWebScreenshot"] = True
@@ -30,30 +30,59 @@ class TestApiDemos(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_api_demos_1(self):
-        el1 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="App")
-        el1.click()
-        el2 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Menu")
-        el2.click()
-        el3 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Inflate from XML")
-        # el3.click()
-        # if el3.text == "Inflate from XML":
-        #     print("SUCCESS")
-        self.assertEqual(el3.text, "Inflate from XML")
+    # def test_api_demos_1(self):
+    #     el1 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="App")
+    #     el1.click()
+    #     el2 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Menu")
+    #     el2.click()
+    #     el3 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Inflate from XML")
+    #     # el3.click()
+    #     # if el3.text == "Inflate from XML":
+    #     #     print("SUCCESS")
+    #     self.assertEqual(el3.text, "Inflate from XML")
+    #
+    # def test_api_demos_2(self):
+    #     el1 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="App")
+    #     el1.click()
+    #     el2 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Menu")
+    #     el2.click()
+    #     el3 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Inflate from XML")
+    #     el3.click()
+    #     title_inflate_from_xml = self.driver.find_element(
+    #         by=AppiumBy.XPATH,
+    #         value="/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TextView"
+    #     )
+    #     self.assertEqual(title_inflate_from_xml.text, "App/Menu/Inflate from XML")
 
-    def test_api_demos_2(self):
-        el1 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="App")
-        el1.click()
-        el2 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Menu")
-        el2.click()
-        el3 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Inflate from XML")
-        el3.click()
-        title_inflate_from_xml = self.driver.find_element(
-            by=AppiumBy.XPATH,
-            value="/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TextView"
+    def test_api_demos_3_drag_and_drop(self):
+        actions = ActionChains(self.driver)
+        actions.w3c_actions = ActionBuilder(
+            self.driver,
+            mouse=PointerInput(interaction.POINTER_TOUCH, "touch")
         )
-        self.assertEqual(title_inflate_from_xml.text, "App/Menu/Inflate from XML")
+        actions.w3c_actions.pointer_action.move_to_location(332, 925)
+        actions.w3c_actions.pointer_action.pointer_down()
+        actions.w3c_actions.pointer_action.move_to_location(339, 454)
+        actions.w3c_actions.pointer_action.release()
+        actions.perform()
 
+        el1 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Views")
+        el1.click()
+        el2 = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Drag and Drop")
+        el2.click()
+
+        # Drag and drop
+        dot_1 = self.driver.find_element(by=AppiumBy.ID, value="io.appium.android.apis:id/drag_dot_1")
+        dot_2 = self.driver.find_element(by=AppiumBy.ID, value="io.appium.android.apis:id/drag_dot_2")
+
+        # TODO: Zmodyfikowac ponizszy kod w celu obslugi przesuniecia z dot_1 do dot_2
+        actions_dnd = ActionChains(self.driver)
+        # actions_dnd.w3c_actions = ActionBuilder(self.driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
+        actions_dnd.move_to_element(dot_1)
+        actions_dnd.click_and_hold(dot_1)
+        actions_dnd.move_to_element(dot_2)
+        actions_dnd.release(dot_2)
+        actions_dnd.perform()
 
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(TestApiDemos)
